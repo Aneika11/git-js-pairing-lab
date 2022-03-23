73e0163c2076073e77e645b5b48e319a9a737a13
khans8890 <kansumaira@gmail.com>
2022-03-23 18:21:03 +0000

    a solved no.2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Code your solutions in this file
+//Q1
 function fiveToOneHundred() {
     for (let i = 5; i <= 100; i++){
         console.log(i)
@@ -6,3 +7,14 @@ function fiveToOneHundred() {
 }
 console.log("Q1")
 fiveToOneHundred()
+
+//Q2
+function multiplesOfThree(){
+    for(let i = 0; i <= 100; i++){ 
+        if(i % 3 === 0){
+            console.log(i)
+        }
+}
+}
+console.log("Q2")
+multiplesOfThree()
