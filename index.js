61587363721f28e557f9d53a5c51b23fdaa36b3d
khans8890 <kansumaira@gmail.com>
2022-03-24 02:50:23 +0000

    solved no. 10

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function multiply(n,num) {
 console.log("Q5")
 console.log(multiply(2, 4));
 
-
+//Q6
 function add(n,num) {
     if(n == num) {
        return 3 * (n + num);
@@ -61,6 +61,7 @@ function add(n,num) {
 console.log("Q6");
 console.log(add(9,9));
 
+//Q7
 function isNegative(num){
     if(num < 1 ){
         return true;
@@ -71,12 +72,14 @@ function isNegative(num){
 console.log("Q7");
 console.log(isNegative(3))
 
+//Q8
 function triangleArea(a,b){
     return (a*b)/2;
 }
 console.log("Q8");
 console.log(triangleArea(2,7));
 
+//Q9
 function betweenTwentyAndFourty(num){
     if(num < 20 || num > 40){
         return true;
@@ -87,3 +90,16 @@ function betweenTwentyAndFourty(num){
 }
 console.log('Q9');
 console.log(betweenTwentyAndFourty(4));
+
+//Q10
+function largest(num1, num2, num3){
+    if (num1 > num2 && num1 > num3){
+        return num1
+    }else if (num2 > num1 && num2 > num3){
+        return num2
+    }else{
+        return num3
+    }
+}
+console.log("Q10");
+console.log(largest(10, 20, 30));
